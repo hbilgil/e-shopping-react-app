1eced5a3758674d7708a54f1ee980ab57bfbde1d
Halil Bilgil <hbilgil@gmail.com>
2022-12-03 14:16:11 +0300

    Add Main component

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Main = () => {
+  return (
+    <MainWrapper>
+
+    </MainWrapper>
+  )
+}
+
+const MainWrapper = styled.div`
+    margin: 0px auto; 
+    box-sizing: border-box;
+    min-height: 78vh;
+`;
+
+export default Main;
