5ec28e7dad39869256470cc6c66939c11940753e
Halil Bilgil <hbilgil@gmail.com>
2022-12-03 14:14:37 +0300

    Add types for definng the lordicon elements

---
+++ src/types/lord-icon.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import lottie from 'lottie-web';
+import {defineElement} from 'lord-icon-element';
+
+// register lottie and define custom element
+defineElement(lottie.loadAnimation);
+
+export type LordIconTrigger =
+  | 'hover'
+  | 'click'
+  | 'loop'
+  | 'loop-on-hover'
+  | 'morph'
+  | 'morph-two-way';
+
+export type LordIconProps = {
+  src?: string;
+  trigger?: LordIconTrigger;
+  size?: string;
+};
+
+export const LordIcon = ({
+  src,
+  trigger,
+  size,
+}: LordIconProps) => {
+  return (
+    <lord-icon
+      src={src}
+      trigger={trigger}
+      style={{
+        width: size,
+        height: size,
+      }}
+    />
+  );
+};
---
+++ src/types/types.ts
@@ -0,0 +1,29 @@
+import React from 'react';
+
+type LordIconTrigger =
+  | 'hover'
+  | 'click'
+  | 'loop'
+  | 'loop-on-hover'
+  | 'morph'
+  | 'morph-two-way';
+
+type LordIconProps = {
+  src?: string;
+  trigger?: LordIconTrigger;
+};
+
+type LordIconElement = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLElement>,
+  HTMLElement
+> &
+  LordIconProps;
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      'lord-icon': LordIconElement;
+    }
+  }
+}
