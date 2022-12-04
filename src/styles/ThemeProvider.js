4e25592708446ed6f2eef48053a1d443758d39c2
Halil Bilgil <hbilgil@gmail.com>
2022-12-04 14:25:36 +0300

    Edit theme provider for about div colors

--- src/styles/ThemeProvider.js
+++ src/styles/ThemeProvider.js
@@ -7,6 +7,9 @@ const Theme = {
       lightFooter: "#f7f7f7",
       footerPara: "#646464",
       linkHover: "#ff613a",
+      aboutText1: "#f5a7a7",
+      aboutText2: "#fad7b0",
+      aboutText3: "#b0d3fa",
     },
 };
   
