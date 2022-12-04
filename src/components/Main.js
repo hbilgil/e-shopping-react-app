797f2d2464224775e364d12aa058663da25bc09e
Halil Bilgil <hbilgil@gmail.com>
2022-12-04 21:13:59 +0300

    Edit main component to see shopping cart component

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 import About from './About/About';
 import Home from './Home/Home';
 import Shop from './Shop/Shop';
+import ShoppingCart from './Shopping Cart/ShoppingCart';
 
 const Main = () => {
   return (
     <MainWrapper>
+      <ShoppingCart/>
       <Shop/>
       <Home/>
       <About/>
