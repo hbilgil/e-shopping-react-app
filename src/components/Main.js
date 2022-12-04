9638d5a82130da487ba6b3ed96c718562bf61d77
Halil Bilgil <hbilgil@gmail.com>
2022-12-04 14:26:05 +0300

    Edit Main component to see changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import About from './About/About';
 
 const Main = () => {
   return (
     <MainWrapper>
-
+      <About></About>
     </MainWrapper>
   )
 }
