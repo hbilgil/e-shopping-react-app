7834b63db8927452f2d5309af29a93c41e5b3efe
Halil Bilgil <hbilgil@gmail.com>
2022-12-05 14:32:11 +0300

    Edit index.js for prime accordion style imports

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from "./styles/GlobalStyle";
 import Theme from "./styles/ThemeProvider";
 import App from './App';
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
